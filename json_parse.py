5309b55b1fc9baf682beb4379318b241001e26c1
Radka Semjanova <radka.semjan@outlook.com>
2024-04-09 14:57:08 +0200

    .

--- json_parse.py
+++ json_parse.py
@@ -1,11 +1,11 @@
 import json
-import os 
+import os
 
 import psycopg
-import logging 
+import logging
+
 
 def parse_json():
- 
 
     current_path = os.path.realpath(__file__)
     current_dir = os.path.dirname(current_path)
@@ -14,81 +14,95 @@ def parse_json():
     f = open(file_path)
     data = json.load(f)
 
-    #hladame portchannel a ethernet 
-    portchannel_data = data["frinx-uniconfig-topology:configuration"]["Cisco-IOS-XE-native:native"]["interface"]["Port-channel"]
-    ten_gb_ethernet = data["frinx-uniconfig-topology:configuration"]["Cisco-IOS-XE-native:native"]["interface"]["TenGigabitEthernet"]
-    gigabitethernet = data["frinx-uniconfig-topology:configuration"]["Cisco-IOS-XE-native:native"]["interface"]["GigabitEthernet"]
+    # hladame portchannel a ethernet
+    portchannel_data = data["frinx-uniconfig-topology:configuration"][
+        "Cisco-IOS-XE-native:native"
+    ]["interface"]["Port-channel"]
+    ten_gb_ethernet = data["frinx-uniconfig-topology:configuration"][
+        "Cisco-IOS-XE-native:native"
+    ]["interface"]["TenGigabitEthernet"]
+    gigabitethernet = data["frinx-uniconfig-topology:configuration"][
+        "Cisco-IOS-XE-native:native"
+    ]["interface"]["GigabitEthernet"]
 
-    
     return portchannel_data, ten_gb_ethernet, gigabitethernet
 
 
-class RadkasDatabase():
+class RadkasDatabase:
 
     def __init__(self) -> None:
-        # parameters to connect to database
-        # self.dbname = "your_database_name"
-        # self.user = "your_username"
-        # self.password = "your_password"
-        # self.host = "localhost"  
-        # self.port = "5432"  
+        self.dbname = "config"
+        self.user = "radkaes"
+        self.password = "postgres"
+        self.host = "127.0.0.1"
+        self.port = "5432"
 
-        # # creating connection to DB
-        # self.conn = psycopg.connect(dbname=self.dbname, user=self.user, password=self.password, host=self.host, port=self.port)
+        self.conn = psycopg.connect(
+            dbname=self.dbname,
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+        )
         print("Connection to DB part ")
+        self.cur = self.conn.cursor()
 
+    def insert_into_table(self, data_list, portchannel=False):
 
-    def insert_into_table(data_list, portchannel = False ):
-        
-        #sql = """INSERT INTO config_data (name, description, max_frame_size, config, port_channel_id, ) VALUES (%s, %s, %s, %s, %s, %s)"""
+        create_table_sql = """
+            CREATE TABLE IF NOT EXISTS port_config (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255),
+                description VARCHAR(255),
+                max_frame_size INTEGER,
+                config JSON,
+                port_channel_id INTEGER
+            )
+        """
 
-        sql_data = []
-
+        # Execute the CREATE TABLE statement
+        self.cur.execute(create_table_sql)
 
         for data in data_list:
-         
             if portchannel:
-                #get ID for current portchannel 
-                port_ids = data['Cisco-IOS-XE-ethernet:service']['instance']
-            
-                for id in port_ids :
-                    port_id  = id['id']
-                    print("ID value:", port_id )
-            else: 
-                port_id = None 
-
-            if 'description' in data:
-                description = data['description']
+                # get ID for current portchannel
+                port_ids = data["Cisco-IOS-XE-ethernet:service"]["instance"]
+                for id in port_ids:
+                    port_id = id["id"]
+                    print("ID value:", port_id)
             else:
-                description = None 
-            
-            if 'mtu' in data:
-                mtu = data['mtu']
+                port_id = None
+
+            if "description" in data:
+                description = data["description"]
             else:
-                mtu = None 
+                description = None
 
-            sql_data.append((data['name'], description, mtu, data, port_id))
+            if "mtu" in data:
+                mtu = data["mtu"]
+            else:
+                mtu = None
 
+            config_json = json.dumps(data)
 
+            self.cur.execute(
+                "INSERT INTO port_config (name, description, max_frame_size, config, port_channel_id) VALUES (%s, %s, %s, %s, %s)",
+                (f"{data['name']}", description, mtu, config_json, port_id),
+            )
 
+            self.conn.commit()
+        print("Data inserted successfully!")
 
-        # try:
-        #     cur = self.conn.cursor()
-        #     cur.execute(sql, data)
-        #     self.conn.commit()
-        #     print("Data inserted successfully!")
+        # Close the cursor and connection
 
-        # except Exception as e:
-        #     # Rollback the transaction in case of an error
-        #     print("Problem")
-            
-        # # Close the cursor and connection
-        # cur.close()
-        # self.conn.close()
+    def close_connection_db(self):
+        self.cur.close()
+        self.conn.close()
 
 
 portchannel_data, ten_gb_ethernet, gigabitethernet = parse_json()
-r_db=RadkasDatabase
+r_db = RadkasDatabase()
 r_db.insert_into_table(portchannel_data, portchannel=True)
 r_db.insert_into_table(ten_gb_ethernet)
 r_db.insert_into_table(gigabitethernet)
+r_db.close_connection_db()
